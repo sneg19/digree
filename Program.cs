34152d3bedbb519148ed4df067c5a7e494e116be
sneg <sneg.sbg@gmail.com>
2023-01-29 15:24:35 +0300

    d

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите функцию, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// Нельзя использовать библиотеку Math
+
+int Degree(int A, int B)
+{
+    int count = 1;
+    int result = A;
+    for (result = A; count < B; count++)
+    {
+        result = result * A;
+    }
+    return result; 
+}
+
+Console.WriteLine("Введите число A: ");
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число B: ");
+int B = int.Parse(Console.ReadLine()!);
+int result = Degree(A, B);
+Console.WriteLine(result);
+
+
+
